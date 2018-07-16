5e4d7f4b0c994685508b8f522298e2c632b2e3ab
henrique221 <hborges9294@gmail.com>
2018-07-16 17:16:26 -0300

    burndown.js

--- src/index.js
+++ src/index.js
@@ -10,14 +10,21 @@ app.set('view engine', 'html');
 app.set('views', path.join(__dirname, '/views/'));
 app.use('/static', express.static(path.join(__dirname, '../public/')));
 
-router.get('/app', (req, res) => {
+router.get('/home', (req, res) => {
     res.render(
-        'sec.html',
+        'index.html',
         {}
     );
 });
 
-app.use('/ms', router);
+router.get('/burndown', (req, res) => {
+    res.render(
+        'burndown.html',
+        {}
+    );
+});
+
+app.use('/', router);
 
 app.listen(8080, () => {
     process.stdout.write(`Starting the ms application on port 8080 with development environment`);
