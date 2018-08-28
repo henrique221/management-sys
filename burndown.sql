9a7a234897016e4bef76c3415851f4fbd36ad1a5
henrique221 <hborges9294@gmail.com>
2018-08-28 16:14:25 -0300

    database migrations

--- burndown.sql
+++ burndown.sql
@@ -29,14 +29,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `progresso` (
-  `id_progresso` int(11) NOT NULL,
+  `id_progresso` int(11) NOT NULL PRIMARY KEY,
   `id_sprint` int(11) NOT NULL,
   `data` date DEFAULT NULL,
   `remaining_tasks` int(11) DEFAULT NULL,
   `bugs` int(11) DEFAULT NULL,
   `improvements` int(11) DEFAULT NULL,
-  `extra_tasks` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `extra_tasks` int(11) DEFAULT NULL,
+  FOREIGN KEY (`id_sprint`) REFERENCES `sprint` (`id`));
+  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -45,52 +46,13 @@ CREATE TABLE `progresso` (
 --
 
 CREATE TABLE `sprint` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY,
   `nome` varchar(100) DEFAULT NULL,
   `start_date` date DEFAULT NULL,
   `days` int(11) DEFAULT NULL,
   `total_tasks` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Índices de tabela `progresso`
---
-ALTER TABLE `progresso`
-  ADD PRIMARY KEY (`id_progresso`),
-  ADD KEY `id_sprint` (`id_sprint`);
-
---
--- Índices de tabela `sprint`
---
-ALTER TABLE `sprint`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
---
-
---
--- AUTO_INCREMENT de tabela `progresso`
---
-ALTER TABLE `progresso`
-  MODIFY `id_progresso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=133;
-
---
--- AUTO_INCREMENT de tabela `sprint`
---
-ALTER TABLE `sprint`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=121;
-
---
--- Restrições para dumps de tabelas
---
-
---
--- Restrições para tabelas `progresso`
---
-ALTER TABLE `progresso`
-  ADD CONSTRAINT `progresso_ibfk_1` FOREIGN KEY (`id_sprint`) REFERENCES `sprint` (`id`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
