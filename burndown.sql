a8501675bc0cca615691983380bbbd4e67ca5258
henrique221 <hborges9294@gmail.com>
2018-08-28 16:25:31 -0300

    database migrations

--- burndown.sql
+++ burndown.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Tempo de geração: 28/08/2018 às 16:38
+-- Tempo de geração: 28/08/2018 às 19:22
 -- Versão do servidor: 5.6.41
 -- Versão do PHP: 7.2.6
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `burndown`
 --
+CREATE DATABASE IF NOT EXISTS `burndown` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `burndown`;
 
 -- --------------------------------------------------------
 
@@ -28,31 +30,30 @@ SET time_zone = "+00:00";
 -- Estrutura para tabela `progresso`
 --
 
-CREATE TABLE `progresso` (
-  `id_progresso` int(11) NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS `progresso` (
+  `id_progresso` int(11) NOT NULL AUTO_INCREMENT,
   `id_sprint` int(11) NOT NULL,
   `data` date DEFAULT NULL,
   `remaining_tasks` int(11) DEFAULT NULL,
   `bugs` int(11) DEFAULT NULL,
   `improvements` int(11) DEFAULT NULL,
   `extra_tasks` int(11) DEFAULT NULL,
-  FOREIGN KEY (`id_sprint`) REFERENCES `sprint` (`id`));
-  ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+  PRIMARY KEY (`id_progresso`),
+  FOREIGN KEY (`id_sprint`) REFERENCES `sprint` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=133 DEFAULT CHARSET=latin1;
 
 --
 -- Estrutura para tabela `sprint`
 --
 
-CREATE TABLE `sprint` (
-  `id` int(11) NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS `sprint` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) DEFAULT NULL,
   `start_date` date DEFAULT NULL,
   `days` int(11) DEFAULT NULL,
-  `total_tasks` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+  `total_tasks` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=121 DEFAULT CHARSET=latin1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
