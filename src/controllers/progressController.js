0510463e63915d1f1127e93b46fd26c305cc3931
henrique221 <hborges9294@gmail.com>
2018-10-05 09:14:29 -0300

    Added progress controller

--- src/controllers/progressController.js
+++ src/controllers/progressController.js
@@ -9,6 +9,20 @@ class ProgressController{
         this.progressRepository.delete(req.params.id);
         res.redirect('/home');
     }
+    selectAll(req, res, next) {
+        const callback = callback;
+        this.progressRepository.selectAll(req.params.sprintId, function(err, callback){
+            if (err) {
+                next(err)
+            } else {
+                const callback = callback
+            }
+            res.render('progressList.html', {
+                    callback
+                }
+            )
+        });
+    }
 }
 
 module.exports = ProgressController
