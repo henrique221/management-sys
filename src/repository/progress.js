0188ec5fc50a088500b41e94f9ee962e325c91e9
henrique221 <hborges9294@gmail.com>
2018-10-05 09:14:02 -0300

    Added progress repository

--- src/repository/progress.js
+++ src/repository/progress.js
@@ -1,4 +1,4 @@
-class Progress{
+class Progress {
     constructor(
         connection
     ) {
@@ -17,6 +17,18 @@ class Progress{
                 }
             }
         );
+    }
+    selectAll(sprintId, callback) {
+        this.connection.query(
+            `SELECT * FROM progresso WHERE id_sprint = ${sprintId}`,
+            function (err, rows) {
+                if (err) {
+                    console.error('error connecting: ' + err.stack);
+                    return log(`Query failed`, err, query);
+                } else
+                    callback(null, rows)
+            }
+        )
     }
 }
 
