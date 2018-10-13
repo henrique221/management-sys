687aefba3cba9c198807576cb9c59e5de57cd657
henrique221 <hborges9294@gmail.com>
2018-10-13 03:25:22 -0300

    Added sprint repository changes to progress controller

--- src/controllers/factories/progressController.js
+++ src/controllers/factories/progressController.js
@@ -1,12 +1,14 @@
 const ProgressController = require('../progressController');
 const ProgressRepository = require('../../repository/factories/progress').make();
 const DateService = require('../../services/factories/dateserviceFactory').make();
+const SprintRepository = require('../../repository/factories/sprint').make();
 
 class ProgressControllerFactory {
     static make() {
         return new ProgressController(
             ProgressRepository,
-            DateService
+            DateService,
+            SprintRepository
         )
     }
 }
--- src/controllers/progressController.js
+++ src/controllers/progressController.js
@@ -1,12 +1,14 @@
-class ProgressController{
+class ProgressController {
     constructor(
         progressRepository,
-        dateService
+        dateService,
+        sprintRepository
     ) {
         this.progressRepository = progressRepository;
         this.dateService = dateService
+        this.sprintRepository = sprintRepository
     }
-    
+
     delete(req, res, next) {
         this.progressRepository.delete(req.params.id);
         res.redirect('/home');
@@ -14,13 +16,20 @@ class ProgressController{
     get(req, res, next) {
         this.progressRepository.selectAllBySprintId(req.params.sprintId, (results) => {
             results.map((item) => {
-                item.data = this.dateService.simpleFormat(item.data, 'DD/MM/YYYY');
+                item.data = this.dateService.simpleFormat(item.data, 'YYYY-MM-DD');
+                item.formatData = this.dateService.simpleFormat(item.data, 'DD/MM/YYYY - dddd');
             });
-            res.render(
-                'progressList.html', {
-                    results
-                }
-            );
+            this.sprintRepository.selectSprintFromSprintTable((resultsList) => {
+                const modalActionForProgress = "Edit progress"
+                console.log(resultsList)
+                res.render(
+                    'progressList.html', {
+                        results,
+                        modalActionForProgress,
+                        resultsList
+                    }
+                );
+            });
         });
     }
 }
